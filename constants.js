87b4c89e1ab00549138a318a319f5d41bd736185
aunnnn <aun.wirawit@gmail.com>
2017-09-24 02:54:46 +0700

    Add constants file

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const GC_USER_ID = 'graphcool-user-id'
+export const GC_AUTH_TOKEN = 'graphcool-auth-token'
+export const GC_USERNAME = 'graphcool-username'
+export const CHATROOMS_PER_PAGE = 20
