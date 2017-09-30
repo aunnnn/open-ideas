1f745b0f3d3dce934ec4c853cb7bfe0edb729c95
Jirat Onaree <jirat.onaree@gmail.com>
2017-09-30 10:37:47 +0700

    Improve MainLayout

--- layouts/main.js
+++ layouts/main.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Link from 'next/link'
+import { withRouter } from 'next/router'
 import Menu from '../components/Menu'
 import Meta from '../components/meta'
 import { GC_USER_ID } from '../constants'
 
 class MainLayout extends Component {
 
-  render() { 
+  render() {
+    const { pathname } = this.props.router
     return (
       <div className="main">
         { /* global styles and meta tags */ }
@@ -14,7 +16,17 @@ class MainLayout extends Component {
 
         {/* Site content */}
         <div className="sidebar">
-          hey
+          <Link prefetch href="/">
+            <a className="logo">
+              <img src="static/plato-red.jpg" className="logo-img"/>
+              <h1 className="logo-text">Platonos</h1>
+            </a>
+          </Link>
+          <div className="button-wrapper">
+            <Link prefetch href="/"><a className={pathname === '/' && 'active'}>Read</a></Link>
+            <Link prefetch href="/talk"><a className={pathname === '/talk' && 'active'}>Talk</a></Link>
+            <Link prefetch href="/join"><a className={pathname === '/join' && 'active'}>Join</a></Link>
+          </div>
         </div>
         <div className="world">
           { this.props.children }
@@ -28,16 +40,46 @@ class MainLayout extends Component {
           .main {
             display: flex;
             flex-direction: row;
-            background-color: blue;
             height: 100vh;
           }
           .sidebar {
-            flex-basis: 85px;
-            min-width: 85px;
-            border-left: 1px solid #ddd;
+            flex-basis: 98px;
+            min-width: 98px;
+            border-right: 1px solid #ddd;
+          }
+          .logo {
+            pointer: cursor;
+          }
+          .logo-img {
+            margin: 15px 25px 7px;
+            width: 48px;
+          }
+          .logo-text {
+            font-size: 14px;
+            font-weight: bold;
+            text-align: center;
+            margin: 0 0 15px;
+            color: #972952;
+          }
+          .button-wrapper {
+            margin-top: 20px;
+          }
+          .button-wrapper a {
+            color: #000;
+            display: block;
+            text-align: center;
+            padding: 15px 0;
+          }
+          .button-wrapper a:hover {
+            background-color: #fafafa;
+          }
+          .button-wrapper a.active {
+            font-weight: bold;
+            background-color: #f9f9f9;
+            border-top: 1px solid #ddd;
+            border-bottom: 1px solid #ddd;
           }
           .world {
-            background-color: red;
             flex: 1 1 auto;
           }
         `}</style>
@@ -46,4 +88,4 @@ class MainLayout extends Component {
   }
 }
   
-export default MainLayout;
+export default withRouter(MainLayout);
