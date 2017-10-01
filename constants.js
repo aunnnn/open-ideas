64cec7099a7b836ea563c51fd2a4f6b577cff645
Jirat Onaree <jirat.onaree@gmail.com>
2017-10-02 00:35:57 +0700

    MORE CHATS

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 export const GC_USER_ID = 'graphcool-user-id'
 export const GC_AUTH_TOKEN = 'graphcool-auth-token'
 export const GC_USERNAME = 'graphcool-username'
-export const N_CHATROOMS_FIRSTLOAD = 10
-export const N_CHATROOMS_LOADMORE = 5
+export const N_CHATROOMS_FIRSTLOAD = 30
+export const N_CHATROOMS_LOADMORE = 30
