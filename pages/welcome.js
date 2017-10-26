a749ddb29579bb35c6c36f70b2353aea1898ac79
aunnnn <aun.wirawit@gmail.com>
2017-10-26 23:09:58 +0700

    Add welcome page

---
+++ pages/welcome.js
@@ -0,0 +1,104 @@
+
+import Head from "next/head"
+import Link from "next/link"
+import withData from '../lib/withData'
+import Colors from '../utils/Colors'
+
+import React, { Component } from 'react'
+
+class WelcomePage extends Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Welcome to Platonos</title>
+        </Head>
+        <div id="main">
+          <div id="content">
+            {/* <img src="/static/plato-red.jpg" /> */}
+            <h1>Platonos</h1>
+            <h2>Fostering Your Ideas</h2>
+            <div id="bg-wrapper">
+              <img src="/static/loginfooterbg.png" />
+            </div>
+            <div>
+              <Link prefetch href="/"><a className="go-button">{"I'M IN"}</a></Link>
+            </div>
+          </div>
+        </div>
+
+        <style jsx global>{`
+          * {
+            margin: 0;
+            box-sizing: border-box;
+          }
+
+          body {
+            font: 16px monospace;
+            font-weight: 400;
+          }
+
+          #bg-wrapper {
+            min-height: 200px;
+          }
+
+          #bg-wrapper {
+            display: flex;
+            align-items: center;
+          }
+
+          .go-button {
+            margin-top: 12px;
+            font-size: 1.2em;
+            font-weight: bold;
+            text-decoration: none;
+            background: ${Colors.main}
+            color: white;
+            padding: 8px 14px;
+            cursor: pointer;
+          }
+          .go-button:hover {
+            opacity: 0.8;
+          }
+
+          #main {
+            width: 100%;
+            height: 100%;
+            width: 100vw;
+            height: 100vh;
+            margin: 0;
+            position: fixed;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          #content{
+            text-align: center;
+            padding-bottom: 40px;
+          }
+
+          #content img {
+            margin-bottom: 8fpx;
+            width: 100vw;
+            max-width: 1000px;
+            height: auto;
+          }
+        
+          #content h1 {
+            font-size: 50px;
+            color: ${Colors.main};
+          }
+          #content h2 {
+            font-weight: normal;
+            color: gray;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+const WelcomeWithData = withData(WelcomePage)
+export default WelcomeWithData
