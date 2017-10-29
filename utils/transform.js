de7eab7febbb684a0cced4f22ac07e3a57b5fb22
Jirat Onaree <jirat.onaree@gmail.com>
2017-10-29 14:31:28 +0700

    url regex strict -> false

--- utils/transform.js
+++ utils/transform.js
@@ -3,12 +3,12 @@ import urlRegex from 'url-regex'
 export const breakpoint = ':$%_reserved_breakpoint_+^:'
 
 export function insert_anchor(text) {
-  return text.replace(urlRegex(), (match) => {
+  return text.replace(urlRegex({ strict: false }), (match) => {
     return `${breakpoint}${match}${breakpoint}`
   })
     .split(breakpoint)
     .map(splited => {
-      if (splited.match(urlRegex())) {
+      if (splited.match(urlRegex({ strict: false }))) {
         let url = splited
         if (!splited.match(/^[a-zA-Z]+:\/\//)) {
           url = 'http://' + url
